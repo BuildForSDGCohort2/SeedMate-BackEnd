1e8ee06a1311a3f8e2bb9adf89306cc452247e12
bdllhdrss3 <bdllhltydrss@gmail.com>
2020-09-13 16:00:29 +0300

    --amend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 require("dotenv").config();
 const formidableMiddleware = require('express-formidable');
@@ -14,6 +15,7 @@ mongoose.connect(process.env.URI, {
 
 
 // middleware 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(formidableMiddleware());
